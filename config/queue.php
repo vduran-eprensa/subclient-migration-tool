52647bdc52a1020b3bc28620c077ed424a1e4089
Victor Duran <victormanuelduran.vd@gmail.com>
2025-12-28 22:38:43 +0100

    fix

--- config/queue.php
+++ config/queue.php
@@ -37,7 +37,7 @@ return [
 
         'database' => [
             'driver' => 'database',
-            'connection' => env('DB_QUEUE_CONNECTION'),
+            'connection' => 'queuedb',
             'table' => env('DB_QUEUE_TABLE', 'jobs'),
             'queue' => env('DB_QUEUE', 'default'),
             'retry_after' => (int) env('DB_QUEUE_RETRY_AFTER', 90),
